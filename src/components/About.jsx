73f890b4434fe1d56163bc73d5974ac4b9c9886b
EDA <eda@mac-mini-3.local>
2016-08-19 10:28:40 +1200

    added about page with welcome & credits

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,27 @@ import React from 'react'
 export default React.createClass({
   render () {
     return (
-      <p>This is an About page</p>
+      <div className="About">
+        <h4>This is an About page</h4>
+        <p>Welcome to the cobras thesaurus app.</p>
+
+        <hr />
+        <footer>
+          <h5>
+            This app is brought to you by the Cobras of Nikau.
+            <ul>
+              <li>Joolz</li>
+              <li>Kamon</li>
+              <li>Sachin</li>
+              <li>Justin</li>
+            </ul>
+          </h5>
+
+          <h5>
+            <em>Thesaurus service provided by <a href="http://words.bighugelabs.com/">words.bighugelabs.com</a></em>
+          </h5>
+        </footer>
+      </div>
     )
   }
 })
