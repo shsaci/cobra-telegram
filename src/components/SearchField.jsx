9d165de0645a13ca707bb23552f53c29c25a92ad
EDA <eda@mac-mini-6.local>
2016-08-19 15:29:13 +1200

    IT WORKS

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -13,16 +13,26 @@ export default React.createClass({
     }
   },
 
-
   handleRequest (evt) {
+    const word = evt.currentTarget ? evt.currentTarget.value : this.state.content
     request
-      .get(`http://words.bighugelabs.com/api/2/99b9e9de9f7b91269a3fd82d037b9068/${this.inputNode.value}/json`)
+      .get(`http://words.bighugelabs.com/api/2/99b9e9de9f7b91269a3fd82d037b9068/${word}/json`)
       .end ((err, res) => {
-      if (err) {
-        console.error(err.message)
-        return
-      }
-      this.setState({words: JSON.parse(res.text).noun.syn})
+        if (err) {
+          console.error(err.message)
+          return
+        } else {
+
+          const checkSyn = JSON.parse(res.text)
+          let arr = []
+          if (checkSyn.hasOwnProperty('noun')) {
+            arr = arr.concat(checkSyn.noun.syn)
+          }
+          if (checkSyn.hasOwnProperty('verb')) {
+            arr = arr.concat(checkSyn.verb.syn)
+          }
+          this.setState({words: arr})
+        }
       })
   },
 
@@ -32,40 +42,26 @@ export default React.createClass({
     this.setState({content: this.inputNode.value})
   },
 
+  handleKeypress(evt) {
+    if(evt.keyCode !== 13) {
+      return
+    }
+    this.handleRequest(evt)
+  },
+
   render () {
     return(
       <div>
-
         <form>
-
-          <input ref={(node) => this.inputNode = node} type="text" name="Word" placeholder="Enter your word here" onChange={this.handleInput} />
-
-        <br />
-
+          <input ref={(node) => this.inputNode = node} type="text" name="Word" placeholder="Enter your word here" onChange={this.handleInput} onKeyUp={this.handleKeypress}
+            />
+            <br />
           <input type="button" value="Submit" onClick={this.handleRequest} />
-
         </form>
-
         <div>
-
           {this.state.words.map((word, i) => <Synonym word={word} key={i} />)}
-
         </div>
       </div>
     )
   }
 })
-
-// handlePost(evt) {
-//   console.log('getting in there')
-//   request
-//   .post('http://192.168.20.57:3000/api/woofs')
-//   .send({"dog_id":this.state.dog_id,"content":this.state.content})
-//   .end((err, res) => {
-//     if (err) {
-//       console.log(err.message)
-//       return
-//     }
-//     console.log(res)
-//   })
-//  },
